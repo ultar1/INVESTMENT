a68d0112e711889cca305997820cbfd8f09c3342
ultar1 <138212013+ultar1@users.noreply.github.com>
2025-11-15 11:46:18 +0100

    Create keyboards.js

---
+++ services/keyboards.js
@@ -0,0 +1,102 @@
+const i18n = require('./i18n');
+
+// Set locale for i18n
+function setLocale(user) {
+    const lang = user ? user.language : 'en';
+    i18n.setLocale(lang);
+    return i18n;
+}
+
+const getLanguageKeyboard = () => {
+    return {
+        inline_keyboard: [
+            [{ text: "🇬🇧 English", callback_data: "set_lang_en" }, { text: "🇷🇺 Русский", callback_data: "set_lang_ru" }],
+            [{ text: "🇪🇸 Español", callback_data: "set_lang_es" }, { text: "🇫🇷 Français", callback_data: "set_lang_fr" }]
+        ]
+    };
+};
+
+const getMainMenuKeyboard = (user) => {
+    const __ = setLocale(user).__;
+    return {
+        keyboard: [
+            [{ text: __('menu.make_investment') }],
+            [{ text: __('menu.my_investments') }, { text: __('menu.my_balance') }],
+            [{ text: __('menu.referral_program') }, { text: __('menu.faq') }],
+            [{ text: __('menu.support') }]
+        ],
+        resize_keyboard: true
+    };
+};
+
+const getBalanceKeyboard = (user) => {
+    const __ = setLocale(user).__;
+    return {
+        inline_keyboard: [
+            [{ text: __('balance.deposit'), callback_data: "deposit" }, { text: __('balance.withdraw'), callback_data: "withdraw" }],
+            [{ text: __('balance.transactions'), callback_data: "transactions" }]
+        ]
+    };
+};
+
+const getInvestmentPlansKeyboard = (user) => {
+    const __ = setLocale(user).__;
+    return {
+        inline_keyboard: [
+            [{ text: __('plans.plan_1_button'), callback_data: "invest_plan_1" }],
+            [{ text: __('plans.plan_2_button'), callback_data: "invest_plan_2" }],
+            [{ text: __('plans.plan_3_button'), callback_data: "invest_plan_3" }],
+            [{ text: __('plans.plan_4_button'), callback_data: "invest_plan_4" }],
+            [{ text: __('common.back'), callback_data: "back_to_main" }]
+        ]
+    };
+};
+
+// New keyboard to select TRC20 or BEP20
+const getNetworkKeyboard = (user) => {
+    const __ = setLocale(user).__;
+    return {
+         inline_keyboard: [
+            [{ text: "TRC20 (Tron)", callback_data: "set_network_trc20" }, { text: "BEP20 (BSC)", callback_data: "set_network_bep20" }],
+            [{ text: __('common.cancel'), callback_data: "cancel_action" }]
+        ]
+    };
+};
+
+const getCancelKeyboard = (user) => {
+    const __ = setLocale(user).__;
+    return {
+        inline_keyboard: [
+            [{ text: __('common.cancel'), callback_data: "cancel_action" }]
+        ]
+    };
+};
+
+const getBackKeyboard = (user, "back_to_main") => {
+    const __ = setLocale(user).__;
+    return {
+        inline_keyboard: [
+            [{ text: __('common.back'), callback_data: "back_to_main" }]
+        ]
+    };
+};
+
+const getMakeInvestmentButton = (user) => {
+     const __ = setLocale(user).__;
+    return {
+        inline_keyboard: [
+            [{ text: __('menu.make_investment'), callback_data: "show_invest_plans" }]
+        ]
+    };
+}
+
+module.exports = {
+    getLanguageKeyboard,
+    getMainMenuKeyboard,
+    getBalanceKeyboard,
+    getInvestmentPlansKeyboard,
+    getNetworkKeyboard,
+    getCancelKeyboard,
+    getBackKeyboard,
+    getMakeInvestmentButton
+};
