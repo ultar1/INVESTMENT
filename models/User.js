5c7963b584e616ab0e99aa1ae9c8108961c9b23b
ultar1 <138212013+ultar1@users.noreply.github.com>
2025-11-15 11:41:03 +0100

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,61 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../db/database');
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    telegramId: {
+        type: DataTypes.BIGINT, // Use BIGINT for large telegram IDs
+        required: true,
+        unique: true,
+        allowNull: false
+    },
+    firstName: {
+        type: DataTypes.STRING
+    },
+    username: {
+        type: DataTypes.STRING
+    },
+    language: {
+        type: DataTypes.STRING,
+        defaultValue: 'en'
+    },
+    balance: {
+        type: DataTypes.DOUBLE,
+        defaultValue: 0
+    },
+    walletAddress: {
+        type: DataTypes.STRING,
+        defaultValue: null
+    },
+    walletNetwork: {
+        type: DataTypes.STRING, // 'trc20' or 'bep20'
+        defaultValue: null
+    },
+    state: {
+        type: DataTypes.STRING,
+        defaultValue: 'none'
+    },
+    stateContext: {
+        type: DataTypes.JSONB, // Use JSONB for objects
+        defaultValue: {}
+    },
+    referralEarnings: {
+        type: DataTypes.DOUBLE,
+        defaultValue: 0
+    },
+    totalInvested: {
+        type: DataTypes.DOUBLE,
+        defaultValue: 0
+    },
+    totalWithdrawn: {
+        type: DataTypes.DOUBLE,
+        defaultValue: 0
+    }
+    // 'referrerId' will be added by associations
+});
+
+module.exports = User;
